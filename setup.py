8eb44091d77dcadfa70973371cd6d1bd12cfce5b
Jayanth Varma <jrvarma@gmail.com>
2022-02-27 21:30:46 +0530

    Bumped version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='icalcli',
-      version='0.9.7',
+      version='0.9.8',
       maintainer='Jayanth R. Varma',
       maintainer_email='jrvarma@gmail.com',
       description='Icalendar Calendar Command Line Interface',
